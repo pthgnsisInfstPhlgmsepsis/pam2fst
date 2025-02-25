3c9ffcf945f49ad3c9e1c41e72037d6b2f297980
pthgnsisInfstPhlgmsepsis <theojosealmeida@gmail.com>
2025-02-25 12:18:54 -0300

    BAGUNCA

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { PaperProvider } from 'react-native-paper'
+import ViaCep from './ViaCep';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.miku}>mkiu</Text>
-      <Text>vocal loid</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        <ViaCep />
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
