175de7e724e290f928e17f1376074a533e98f374
pthgnsisInfstPhlgmsepsis <theojosealmeida@gmail.com>
2025-02-25 11:17:14 -0300

    mkiu vocal loid

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.miku}>mkiu</Text>
+      <Text>vocal loid</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +18,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  miku: {
+    fontSize: '2em',
+    fontWeight: 'bold',
+    fontStyle: 'italic',
+  }
 });
